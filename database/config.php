758542d342005533fc07f1fc6d67b082139359c2
JacobGlatt <jglatt3@att.net>
2015-12-14 15:11:34 -0600

    fixed user database seeder and migration

--- database/config.php
+++ database/config.php
@@ -1,5 +1,6 @@
 <?php
-define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'kriegerlists');
-define('USER', 'krieger');
-define('PASS', 'admin');
+define("DB_HOST", "127.0.0.1");
+define("DB_NAME", "kriegerslist");
+define("USER", "krieger");
+define("PASS", "admin");
+?>
