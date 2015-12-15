13b08890972b729bb2a5aa4c4ce42525f53a55cb
Sj <sjmrstevens@gmail.com>
2015-12-15 13:29:33 -0600

    added labels for forms

--- public/users.create.php
+++ public/users.create.php
@@ -8,52 +8,44 @@
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
   <title>Krieger's List</title>
   <?php include '../views/partials/navbar.php'; ?>
+	<div class="container">
+    	<div class="page-header">
+   			<h2>Sign Up!</h2>
+   		</div>
+	</div>
 </head>
 
 <body>
 	<div class="container">
 	    <div class="row">
 	        <div class="center-block">
-	            <h2 class="form-signin-heading">Sign up!</h2>
 	            <form action="#" method="post" class="form" role="form">
 	            <div class="row">
 	                <div class="col-xs-6 col-md-6">
 	                    <input class="form-control" name="firstname" placeholder="First Name" type="text"
-	                        required autofocus />
+	                        required autofocus>
 	                </div>
 	                <div class="col-xs-6 col-md-6">
-	                    <input class="form-control" name="lastname" placeholder="Last Name" type="text" required />
+	                    <input class="form-control" name="lastname" placeholder="Last Name" type="text" required>
 	                </div>
 	            </div>
-	            <input class="form-control" name="youremail" placeholder="Your Email" type="email" />
-	            <input class="form-control" name="password" placeholder="New Password" type="password" />
-	            <input class="form-control" name="reenterpassword" placeholder="Re-enter Password" type="password" />
-	            <label for="">Birth Date</label>
-	            <div class="row">
-	                <div class="col-xs-4 col-md-4">
-	                    <select class="form-control">
-	                        <option value="Month">Month</option>
-	                        <option value="Month">January</option>
-	                        <option value="Month">February</option>
-	                    </select>
-	                </div>
-	                <div class="col-xs-4 col-md-4">
-	                    <select class="form-control">
-	                        <option value="Day">Day</option>
-	                    </select>
-	                </div>
-	                <div class="col-xs-4 col-md-4">
-	                    <select class="form-control">
-	                        <option value="Year">Year</option>
-	                    </select>
-	                </div>
-	            </div>
+	            <label for="email" type="email" id="email">E-mail</label>
+	            <input class="form-control" name="youremail" placeholder="Your Email" type="email">
+	            <label for="password" type="password" id="password">Password</label>
+	            <input class="form-control" name="password" placeholder="New Password" type="password">
+	            <label for="reenterpassword" type="reenterpassword" id="reenterpassword">Re-enter password</label>
+	            <input class="form-control" name="reenterpassword" placeholder="Re-enter Password" type="password">
+	            <label for="birthdate" type="text" id="birthdate">Birthdate</label>
+	            <input class="form-control" name="birthdate" placeholder="Birthday" type="date">
+
+
+
 	            <label class="radio-inline">
-	                <input type="radio" name="sex" id="inlineCheckbox1" value="male" />
+	                <input type="radio" name="sex" id="inlineCheckbox1" value="male">
 	                Male
 	            </label>
 	            <label class="radio-inline">
-	                <input type="radio" name="sex" id="inlineCheckbox2" value="female" />
+	                <input type="radio" name="sex" id="inlineCheckbox2" value="female">
 	                Female
 	            </label>
 	            <br />
