ee2ff4d833e87615a2bcd0e156f4da89754aa40b
Sj <sjmrstevens@gmail.com>
2015-12-15 09:40:27 -0600

    switched navbar links to right side

--- public/users.create.php
+++ public/users.create.php
@@ -10,56 +10,58 @@
   <?php include '../views/partials/navbar.php'; ?>
 </head>
 
-<div class="container">
-    <div class="row">
-        <div class="">
-            <h2 class="form-signin-heading">Please sign up</h2>
-            <form action="#" method="post" class="form" role="form">
-            <div class="row">
-                <div class="col-xs-6 col-md-6">
-                    <input class="form-control" name="firstname" placeholder="First Name" type="text"
-                        required autofocus />
-                </div>
-                <div class="col-xs-6 col-md-6">
-                    <input class="form-control" name="lastname" placeholder="Last Name" type="text" required />
-                </div>
-            </div>
-            <input class="form-control" name="youremail" placeholder="Your Email" type="email" />
-            <input class="form-control" name="reenteremail" placeholder="Re-enter Email" type="email" />
-            <input class="form-control" name="password" placeholder="New Password" type="password" />
-            <label for="">Birth Date</label>
-            <div class="row">
-                <div class="col-xs-4 col-md-4">
-                    <select class="form-control">
-                        <option value="Month">Month</option>
-                        <option value="Month">January</option>
-                        <option value="Month">jasdf</option>
-                    </select>
-                </div>
-                <div class="col-xs-4 col-md-4">
-                    <select class="form-control">
-                        <option value="Day">Day</option>
-                    </select>
-                </div>
-                <div class="col-xs-4 col-md-4">
-                    <select class="form-control">
-                        <option value="Year">Year</option>
-                    </select>
-                </div>
-            </div>
-            <label class="radio-inline">
-                <input type="radio" name="sex" id="inlineCheckbox1" value="male" />
-                Male
-            </label>
-            <label class="radio-inline">
-                <input type="radio" name="sex" id="inlineCheckbox2" value="female" />
-                Female
-            </label>
-            <br />
-            <br />
-            <button class="btn btn-lg btn-primary btn-block" type="submit">
-                Sign up</button>
-            </form>
-        </div>
-    </div>
-</div>
+<body>
+	<div class="container">
+	    <div class="row">
+	        <div class="">
+	            <h2 class="form-signin-heading">Please sign up</h2>
+	            <form action="#" method="post" class="form" role="form">
+	            <div class="row">
+	                <div class="col-xs-6 col-md-6">
+	                    <input class="form-control" name="firstname" placeholder="First Name" type="text"
+	                        required autofocus />
+	                </div>
+	                <div class="col-xs-6 col-md-6">
+	                    <input class="form-control" name="lastname" placeholder="Last Name" type="text" required />
+	                </div>
+	            </div>
+	            <input class="form-control" name="youremail" placeholder="Your Email" type="email" />
+	            <input class="form-control" name="reenteremail" placeholder="Re-enter Email" type="email" />
+	            <input class="form-control" name="password" placeholder="New Password" type="password" />
+	            <label for="">Birth Date</label>
+	            <div class="row">
+	                <div class="col-xs-4 col-md-4">
+	                    <select class="form-control">
+	                        <option value="Month">Month</option>
+	                        <option value="Month">January</option>
+	                        <option value="Month">jasdf</option>
+	                    </select>
+	                </div>
+	                <div class="col-xs-4 col-md-4">
+	                    <select class="form-control">
+	                        <option value="Day">Day</option>
+	                    </select>
+	                </div>
+	                <div class="col-xs-4 col-md-4">
+	                    <select class="form-control">
+	                        <option value="Year">Year</option>
+	                    </select>
+	                </div>
+	            </div>
+	            <label class="radio-inline">
+	                <input type="radio" name="sex" id="inlineCheckbox1" value="male" />
+	                Male
+	            </label>
+	            <label class="radio-inline">
+	                <input type="radio" name="sex" id="inlineCheckbox2" value="female" />
+	                Female
+	            </label>
+	            <br />
+	            <br />
+	            <button class="btn btn-lg btn-primary btn-block" type="submit">
+	                Sign up</button>
+	            </form>
+	        </div>
+	    </div>
+	</div>
+</body>
--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -1,16 +1,13 @@
 <nav class="navbar navbar-default navbar-fixed-top">
       <div class="container">
-        <div class="navbar-header">
+        <div class="navbar-header navbar-inner" style="float:right">
           <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
             <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
           </button>
           <a class="navbar-brand" href="index.php">Krieger's List</a>
         </div>
         <div id="navbar" class="collapse navbar-collapse">
-          <ul class="nav navbar-nav">
+          <ul class="nav navbar-nav navbar-inner" style="float:right">
             <li><a href="../ads.create.php">Post Your Ad</a></li>
             <li><a href="../auth.login.php">Login</a></li>
           </ul>
