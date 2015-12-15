a35852840a8a9114b1dd3f91e45a4a19357833c5
Sj <sjmrstevens@gmail.com>
2015-12-15 11:20:45 -0600

    polished html, cleaned up whitespace

--- public/users.show.php
+++ public/users.show.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+
 <head>
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
@@ -6,52 +8,51 @@
   <title>Krieger's List</title>
   <?php include '../views/partials/navbar.php'; ?>
 </head>
-<div class="container">
-    <h1>UsernameHere</h1>
-  	<hr>
-	<div class="row">
-      <!-- left column -->
-      <div class="col-md-3">
-        <div class="text-center">
-          <img src="//placehold.it/100" class="avatar img-circle" alt="avatar">
-          <h3><a href="users.edit.php"> Edit your profile</a></h3>
-        </div>
-      </div>
-      <!-- edit user column -->
-        <div class="col-md-9 personal-info">
-        	<h3>Personal info</h3> 
-        	<div class="well">Here is stuff about the user, whatever they feel like sharing</div>
-		    <div class="row">
-	            <div class="col-md-4 portfolio-item">
-	                <a href="ads.show.php">
-	                    <img class="img-responsive" src="http://placehold.it/700x400" alt="">
-	                </a>
-	                <h3>
-	                    <a href="ads.show.php">Fake crap for sale</a>
-	                </h3>
-	                <p>10 cents a pail.</p>
-	            </div>
-	            <div class="col-md-4 portfolio-item">
-	                <a href="ads.show.php">
-	                    <img class="img-responsive" src="http://placehold.it/700x400" alt="">
-	                </a>
-	                <h3>
-	                    <a href="#">Old item</a>
-	                </h3>
-	                <p>Item Description.</p>
-	            </div>
-	            <div class="col-md-4 portfolio-item">
-	                <a href="ads.show.php">
-	                    <img class="img-responsive" src="http://placehold.it/700x400" alt="">
-	                </a>
-	                <h3>
-	                    <a href="#">Weird, used item</a>
-	                </h3>
-	                <p>Item description</p>
-	            </div>
+<body>
+	<div class="container">
+	    <h1>UsernameHere</h1>
+	  	<hr>
+		<div class="row">
+	      <div class="col-md-3">
+	        <div class="text-center">
+	          <img src="//placehold.it/100" class="avatar img-circle" alt="avatar">
+	          <h3><a href="users.edit.php"> Edit your profile</a></h3>
 	        </div>
-
-
-       	</div>
-    </div>
-</div>
+	      </div>
+	        <div class="col-md-9 personal-info">
+	        	<h3>Personal info</h3>
+	        	<div class="well">Here is stuff about the user, whatever they feel like sharing</div>
+			    <div class="row">
+		            <div class="col-md-4 portfolio-item">
+		                <a href="ads.show.php">
+		                	<img class="img-responsive" src="http://placehold.it/700x400" alt="">
+		                </a>
+		                <h3>
+		                    <a href="ads.show.php">Fake crap for sale</a>
+		                </h3>
+		                <p>10 cents a pail.</p>
+		            </div>
+		            <div class="col-md-4 portfolio-item">
+		                <a href="ads.show.php">
+		                    <img class="img-responsive" src="http://placehold.it/700x400" alt="">
+		                </a>
+		                <h3>
+		                    <a href="#">Old item</a>
+		                </h3>
+		                <p>Item Description.</p>
+		            </div>
+		            <div class="col-md-4 portfolio-item">
+		                <a href="ads.show.php">
+		                    <img class="img-responsive" src="http://placehold.it/700x400" alt="">
+		                </a>
+		                <h3>
+		                    <a href="#">Weird, used item</a>
+		                </h3>
+		                <p>Item description</p>
+		            </div>
+		        </div>
+	       	</div>
+	    </div>
+	</div>
+	<?php include '../views/partials/footer.php'; ?>
+</body>
--- views/partials/header.php
+++ views/partials/header.php
@@ -2,6 +2,5 @@
     <div class="page-header">
    		<h1>Site for selling weird stuff.</h1>
     </div>
-    <p class="lead">Here is a description of our site to buy weird stuff online. Maybe put in an search form to search by location here.</p>
-
+    <p class="lead">Here is a description of our site to buy weird stuff online.</p>
 </div>
