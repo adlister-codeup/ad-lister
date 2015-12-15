af9216b7012393f78cbef39e6a11ad6bc25cddc7
Jacob <jacob@MBP.local>
2015-12-15 10:33:20 -0600

    added migration and seeder for ads

---
+++ database/adsmigration.php
@@ -0,0 +1,21 @@
+<?php
+	require_once 'private.php';
+	$dbc = new PDO('mysql:host=' . DB_HOST . ';dbname=' . DB_NAME, USER, PASS);
+	$dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+	$query = 'DROP TABLE IF EXISTS ads';
+	$dbc->exec($query);
+
+	$table = "CREATE TABLE IF NOT EXISTS ads (
+		id INT UNSIGNED NOT NULL AUTO_INCREMENT,
+		title VARCHAR(200) NOT NULL,
+		description TEXT NOT NULL,
+		email VARCHAR(100) NOT NULL,
+		phone BIGINT,
+		price FLOAT NOT NULL,
+		location VARCHAR(250) NOT NULL,
+		images TEXT,
+		PRIMARY KEY (id)
+	)";
+	$dbc->exec($table);
+?>
