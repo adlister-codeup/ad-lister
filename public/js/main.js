1520f549e92c4a3966b4553dfd649e108609db83
JacobGlatt <jglatt3@att.net>
2015-12-16 10:56:42 -0600

    added loading from database

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,4 @@
+"use strict";
 (function(){
 	$.ajax ({ // get request to load the contacts from the users file.
 	    type: "GET",
@@ -15,6 +16,28 @@
 	});
 	function fillAds(data) {
 		console.log(data);
+		var length = data.length;
+		var rows = length / 3;
+		var dataLocation = 0;
+		for (var rowplace = 0; rowplace < rows; rowplace++) {
+			var row = $('<div class="row">').addClass("rowplace"+rowplace);
+			$("#ads").append(row);
+			for (var div = 0; div < 3; div++) {
+				var divopen = $('<div class="col-md-4 portfolio-item">').addClass("divof"+dataLocation);
+				var linkopen = $('<a href="ads.show.php">').attr("href", "ads.show.php?ad="+data[dataLocation].id).addClass("link"+dataLocation);
+				var image = $('<img class="img-responsive" src="http://placehold.it/700x400" alt="">').attr("src", "http://placehold.it/700x400");//data[0].images[0]);
+				var h3 = $('<h3></h3').addClass("h3of"+dataLocation);
+				var titlelink = $('<a href="ads.show.php"></a>').attr("href", "ads.show.php?ad="+data[dataLocation].id).text(data[dataLocation].title);
+				var desc = $('<p></p>').text(data[dataLocation].description);
+				$(".rowplace"+rowplace).append(divopen);
+				$(".divof"+dataLocation).append(linkopen);
+				$(".link"+dataLocation).append(image);
+				$(".divof"+dataLocation).append(h3);
+				$(".divof"+dataLocation).append(linkopen);
+				$(".h3of"+dataLocation).append(titlelink);
+				$(".divof"+dataLocation).append(desc);
+				dataLocation++;
+			}
+		}
 	}
-
 })();
