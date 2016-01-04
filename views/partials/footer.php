6c587672984ea109f60add4e55d133dbc16b3f3e
JacobGlatt <jglatt3@att.net>
2016-01-04 13:49:52 -0600

    login/logout

--- views/partials/footer.php
+++ views/partials/footer.php
@@ -1,6 +1,5 @@
 <footer class="footer">
     <div class="nav navbar-nav" style="float:right">
     	<button type="button" class="btn"><a href="../users.edit.php">Edit Your Profile</a></button>
-    	<button type="button" class="btn"><a href="../auth.logout.php">Logout</a></button>
     </div>
 </footer>
--- views/partials/navbar.php
+++ views/partials/navbar.php
@@ -13,7 +13,11 @@
     <div id="navbar" class="collapse navbar-collapse">
       <ul class="nav navbar-nav navbar-inner" style="float:right">
         <button type="button" class="btn"><li><a href="../ads.create.php">Post Your Ad</a></li></button>
-        <button type="button" class="btn"><li><a href="../auth.login.php">Login</a></li></button>
+          <?php if (isset($_SESSION["user"])) {?>
+          <button type="button" class="btn"><a href="../auth.logout.php">Logout</a></button>
+        <?php } else { ?>
+          <button type="button" class="btn"><li><a href="../auth.login.php">Login</a></li></button>
+        <?php } ?>
         <button type="button" class="largebtn"><li><a href="index.php">Krieger's List</a></li></button>
       </ul>
       <div>Welcome: <?= $user ?></div>
